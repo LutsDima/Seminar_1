f9264dbac1296ec9e358ff1591691d24a7fcdaf9
«Dmitriy» <dima_luts@mail.ru>
2023-03-30 10:55:17 +0300

    Добавлен код первой задачи

--- Dz_Seminar_1/Program.cs
+++ Dz_Seminar_1/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+Console.WriteLine("Введите первое число: ");
+double Number_a = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+double Number_b = Convert.ToDouble(Console.ReadLine());
+
+if (Number_a > Number_b)
+{
+    Console.WriteLine($"max = {Number_a} ");
+}
+else if (Number_a < Number_b)
+{
+    Console.WriteLine($"max = {Number_b} ");
+}
+else 
+{
+    Console.WriteLine("Числа равны");
+}
