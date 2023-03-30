ad0ac5d8bbf325c7c8e18c82401d4ffc42e972c9
«Dmitriy» <dima_luts@mail.ru>
2023-03-30 10:17:18 +0300

    inital commit

---
+++ Seminar_1/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Введите число: ");
+string strCount = Console.ReadLine();
+int numb = Convert.ToInt32(strCount);
+Console.WriteLine($"Квадрат числа = {numb * numb}");
+//Console.WriteLine(numb*numb);
+
+Console.WriteLine("Введите число дня недели ");
