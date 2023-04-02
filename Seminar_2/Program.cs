709672ef0c849b5cadeb40e42c04829560619520
«Dmitriy» <dima_luts@mail.ru>
2023-04-02 23:27:38 +0300

    add Zadacha 4

--- Seminar_2/Program.cs
+++ Seminar_2/Program.cs
@@ -18,17 +18,42 @@
 
 // Console.WriteLine($"{num1}{num2}");
 
-Console.Clear();
+// Console.Clear();
 
-Console.WriteLine("Введите 2 числа");
-int num1 = int.Parse(Console.ReadLine());
-int num2 = int.Parse(Console.ReadLine());
+// Console.WriteLine("Введите 2 числа");
+// int num1 = int.Parse(Console.ReadLine());
+// int num2 = int.Parse(Console.ReadLine());
 
-if (num1 % num2 == 0)
-{
-    Console.WriteLine($"{num1} кратно {num2}");
-} 
-else
-{
-    Console.WriteLine($"{num1 % num2}");
-}
+// if (num1 % num2 == 0)
+// {
+//     Console.WriteLine($"{num1} кратно {num2}");
+// } 
+// else
+// {
+//     Console.WriteLine($"{num1 % num2}");
+// }
+
+// Console.WriteLine("Enter number");
+// int num = int.Parse(Console.ReadLine());
+
+// int num1 = num % 7;
+// int num2 =num % 23;
+// if (num % 7 == 0)
+// {
+//     if (num % 23 == 0);
+//     {
+//         Console.WriteLine("Yes");
+//     }
+// } 
+// else Console.WriteLine("No");
+
+
+// Console.WriteLine("Введите 2 числа: ");
+// int num1 = int.Parse(Console.ReadLine());
+// int num2 = int.Parse(Console.ReadLine());
+
+// if (num1 = num2*num2 || num2 = num1*num1)
+// {
+//     Console.WriteLine("-> да");
+// }
+// else Console.WriteLine("-> no");
