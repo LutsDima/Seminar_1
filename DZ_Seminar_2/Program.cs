02fb98e55f80c09949fc76c35cdb2c9a3ff3c5cb
«Dmitriy» <dima_luts@mail.ru>
2023-04-02 23:50:25 +0300

    Add Задача 15

--- DZ_Seminar_2/Program.cs
+++ DZ_Seminar_2/Program.cs
@@ -3,13 +3,13 @@
 // 782 -> 8
 // 918 -> 1
 
-Console.Clear();
-Console.WriteLine("Введите трехзначное число: ");
-int num = int.Parse(Console.ReadLine());
+// Console.Clear();
+// Console.WriteLine("Введите трехзначное число: ");
+// int num = int.Parse(Console.ReadLine());
 
-int num1 = (num % 100) / 10;
+// int num1 = (num % 100) / 10;
 
-Console.WriteLine($"{num1}");
+// Console.WriteLine($"{num1}");
 
 // Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
 // 645 -> 5
@@ -24,4 +24,16 @@ Console.Clear();
 // 1 -> нет
 
 Console.Clear();
-Console.WriteLine()
+Console.WriteLine("Введите номер дня недели");
+int num = int.Parse(Console.ReadLine());
+
+switch (num)
+{
+    case 1: Console.WriteLine("NO"); break;
+    case 2: Console.WriteLine("NO"); break;
+    case 3: Console.WriteLine("NO"); break;
+    case 4: Console.WriteLine("NO"); break;
+    case 5: Console.WriteLine("NO"); break;
+    case 6: Console.WriteLine("Yes"); break;
+    case 7: Console.WriteLine("Yes"); break;
+}
