86f33bc66b032537217345d2efd3311dddd0709c
«Dmitriy» <dima_luts@mail.ru>
2023-04-03 00:11:14 +0300

    Add Задача 13

--- DZ_Seminar_2/Program.cs
+++ DZ_Seminar_2/Program.cs
@@ -18,22 +18,38 @@
 
 Console.Clear();
 
+Console.WriteLine("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+
+if (num < 100)
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+else
+{
+    while (num > 999)
+    {
+        num = num / 10;
+    }
+    Console.WriteLine(num % 10);
+}
+
 // Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
 // 6 -> да
 // 7 -> да
 // 1 -> нет
 
-Console.Clear();
-Console.WriteLine("Введите номер дня недели");
-int num = int.Parse(Console.ReadLine());
+// Console.Clear();
+// Console.WriteLine("Введите номер дня недели");
+// int num = int.Parse(Console.ReadLine());
 
-switch (num)
-{
-    case 1: Console.WriteLine("NO"); break;
-    case 2: Console.WriteLine("NO"); break;
-    case 3: Console.WriteLine("NO"); break;
-    case 4: Console.WriteLine("NO"); break;
-    case 5: Console.WriteLine("NO"); break;
-    case 6: Console.WriteLine("Yes"); break;
-    case 7: Console.WriteLine("Yes"); break;
-}
+// switch (num)
+// {
+//     case 1: Console.WriteLine("NO"); break;
+//     case 2: Console.WriteLine("NO"); break;
+//     case 3: Console.WriteLine("NO"); break;
+//     case 4: Console.WriteLine("NO"); break;
+//     case 5: Console.WriteLine("NO"); break;
+//     case 6: Console.WriteLine("Yes"); break;
+//     case 7: Console.WriteLine("Yes"); break;
+// }
