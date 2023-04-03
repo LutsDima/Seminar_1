3a92b9ee098846cbe58500d02870fda956b69512
«Dmitriy» <dima_luts@mail.ru>
2023-04-03 23:45:14 +0300

    Добавлено решение задачи 23

--- DZ_Seminar_3/Program.cs
+++ DZ_Seminar_3/Program.cs
@@ -9,29 +9,41 @@
 // A (3,6,8); B (2,1,-7), -> 15.84
 // A (7,-5, 0); B (1,-1,9) -> 11.53
 
-Console.Clear();
+// Console.Clear();
 
-Console.WriteLine("Введите координаты первой точки: ");
-Console.Write("X1: ");
-double numX1 = Convert.ToDouble(Console.ReadLine());
-Console.Write("Y1: ");
-double numY1 = Convert.ToDouble(Console.ReadLine());
-Console.Write("Z1: ");
-double numZ1 = Convert.ToDouble(Console.ReadLine());
-Console.WriteLine("Введите координаты второй точки: ");
-Console.Write("X2: ");
-double numX2 = Convert.ToDouble(Console.ReadLine());
-Console.Write("Y2: ");
-double numY2 = Convert.ToDouble(Console.ReadLine());
-Console.Write("Z2: ");
-double numZ2 = Convert.ToDouble(Console.ReadLine());
+// Console.WriteLine("Введите координаты первой точки: ");
+// Console.Write("X1: ");
+// double numX1 = Convert.ToDouble(Console.ReadLine());
+// Console.Write("Y1: ");
+// double numY1 = Convert.ToDouble(Console.ReadLine());
+// Console.Write("Z1: ");
+// double numZ1 = Convert.ToDouble(Console.ReadLine());
+// Console.WriteLine("Введите координаты второй точки: ");
+// Console.Write("X2: ");
+// double numX2 = Convert.ToDouble(Console.ReadLine());
+// Console.Write("Y2: ");
+// double numY2 = Convert.ToDouble(Console.ReadLine());
+// Console.Write("Z2: ");
+// double numZ2 = Convert.ToDouble(Console.ReadLine());
 
-double sum = Math.Sqrt(Math.Pow((numX2-numX1),2) + Math.Pow((numY2-numY1),2) + Math.Pow((numZ2-numZ1),2));
+// double sum = Math.Sqrt(Math.Pow((numX2-numX1),2) + Math.Pow((numY2-numY1),2) + Math.Pow((numZ2-numZ1),2));
 
-Console.WriteLine($"{sum}");
-Console.ReadKey();
+// Console.WriteLine($"{sum}");
+// Console.ReadKey();
 
 // Задача 23
 // Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
 // 3 -> 1, 8, 27
 // 5 -> 1, 8, 27, 64, 125
+
+Console.Clear();
+
+Console.WriteLine("Введите число :");
+int num = int.Parse(Console.ReadLine());
+
+for (int i = 1; i <= num; i++)
+{
+    Console.Write($"{i*i*i}, ");
+}
+
+Console.ReadKey();
