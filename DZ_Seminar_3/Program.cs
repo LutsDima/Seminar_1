8fb3d98d45484756b4ae04ad3b5ad07156faff90
«Dmitriy» <dima_luts@mail.ru>
2023-04-04 00:38:40 +0300

    Добавлено решение задачи 19

--- DZ_Seminar_3/Program.cs
+++ DZ_Seminar_3/Program.cs
@@ -4,32 +4,69 @@
 // 12821 -> да
 // 23432 -> да
 
+Console.Clear();
+bool palindrome (int Number)
+{
+    if (Number / 10000 == Number % 10 && Number % 100 / 10 == Number / 1000 % 10)
+    {
+        return true;
+    }
+    return false;
+}
+
+bool validNumber (int numb)
+{
+    if (numb < 100000 && numb > 9999)
+    {
+        return true;
+    }
+    Console.WriteLine("Это не пятизначное число");
+    return false;
+}
+
+Console.WriteLine("Введите пятизначное число: ");
+int num = int.Parse(Console.ReadLine());
+
+if (validNumber(num))
+{
+    if (palindrome(num))
+    {
+        Console.WriteLine($"Число {num} является палиндромом");
+    }
+    else
+    {
+        Console.WriteLine($"Число {num} не является палиндромом");
+    }
+}
+
+Console.ReadKey();
+
 // Задача 21
 // Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
 // A (3,6,8); B (2,1,-7), -> 15.84
 // A (7,-5, 0); B (1,-1,9) -> 11.53
 
-// Console.Clear();
+Console.Clear();
 
-// Console.WriteLine("Введите координаты первой точки: ");
-// Console.Write("X1: ");
-// double numX1 = Convert.ToDouble(Console.ReadLine());
-// Console.Write("Y1: ");
-// double numY1 = Convert.ToDouble(Console.ReadLine());
-// Console.Write("Z1: ");
-// double numZ1 = Convert.ToDouble(Console.ReadLine());
-// Console.WriteLine("Введите координаты второй точки: ");
-// Console.Write("X2: ");
-// double numX2 = Convert.ToDouble(Console.ReadLine());
-// Console.Write("Y2: ");
-// double numY2 = Convert.ToDouble(Console.ReadLine());
-// Console.Write("Z2: ");
-// double numZ2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите координаты первой точки: ");
+Console.Write("X1: ");
+double numX1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Y1: ");
+double numY1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Z1: ");
+double numZ1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите координаты второй точки: ");
+Console.Write("X2: ");
+double numX2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Y2: ");
+double numY2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Z2: ");
+double numZ2 = Convert.ToDouble(Console.ReadLine());
 
-// double sum = Math.Sqrt(Math.Pow((numX2-numX1),2) + Math.Pow((numY2-numY1),2) + Math.Pow((numZ2-numZ1),2));
+double sum = Math.Sqrt(Math.Pow((numX2-numX1),2) + Math.Pow((numY2-numY1),2) + Math.Pow((numZ2-numZ1),2));
 
-// Console.WriteLine($"{sum}");
-// Console.ReadKey();
+Console.WriteLine($"{sum}");
+Console.ReadKey();
 
 // Задача 23
 // Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
@@ -39,7 +76,7 @@
 Console.Clear();
 
 Console.WriteLine("Введите число :");
-int num = int.Parse(Console.ReadLine());
+num = int.Parse(Console.ReadLine());
 
 for (int i = 1; i <= num; i++)
 {
