eea5789b8afcc71b8d37f2663131e5304e4338e8
«Dmitriy» <dima_luts@mail.ru>
2023-04-10 19:09:59 +0300

    initial commit

--- Seminar_4/Program.cs
+++ Seminar_4/Program.cs
@@ -1,1 +1,10 @@
-﻿
+﻿Console.Clear();
+
+int setNumber (string text)
+{
+    Console.WriteLine("Введите число");
+    int num = int.Parse(Console.ReadLine());
+    return num;
+}
+
+Console.WriteLine 
